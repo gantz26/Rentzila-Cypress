fb7de3460175dd5d9e1c25bfa4d2ff75ceeb75e8
Bohdan Honchar <bohdan26062003@gmail.com>
2024-10-31 14:44:47 +0200

    Code refactoring

--- cypress/helpers/unit_images.api.ts
+++ cypress/helpers/unit_images.api.ts
@@ -3,29 +3,18 @@ class UnitImagesAPI {
         return cy.get("@userAccessToken").then((token) => {
             cy.fixture(filePath, "base64").then((photo) => {
                 const formData = new FormData();
-                const blob = Cypress.Blob.base64StringToBlob(photo, "image/jpeg");
+                const blob = Cypress.Blob.base64StringToBlob(photo, "image/jpg");
                 formData.append("unit", id.toString());
                 formData.append("image", blob, filePath.split('/').pop());
                 formData.append("is_main", isMain ? "true" : "false");
 
-                return new Promise((resolve, reject) => {
-                    const xhr = new XMLHttpRequest();
-                    xhr.open("POST", "https://dev.rentzila.com.ua/api/unit-images/");
-                    xhr.setRequestHeader("Authorization", `Bearer ${token}`);
-
-                    xhr.onload = () => {
-                        if (xhr.status === 200 || xhr.status === 201) {
-                            resolve({ status: xhr.status, response: JSON.parse(xhr.response) });
-                        } else {
-                            console.error("Request failed", {
-                                status: xhr.status,
-                                response: xhr.responseText
-                            });
-                            reject(`Request failed with status ${xhr.status}`);
-                        }
-                    };
-                    xhr.onerror = () => reject("Network error");
-                    xhr.send(formData);
+                return cy.request({
+                    method: "POST",
+                    url: `https://dev.rentzila.com.ua/api/unit-images/`,
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    },
+                    body: formData
                 });
             });
         });
